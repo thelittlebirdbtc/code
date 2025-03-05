22ee17cc40e4142517bc0f161b999ca442c3524f
thelittlebirdbtc <thelittlebirdbtc@outlook.com>
2025-03-05 16:37:37 -0300

    Enhance NotFound component; improve layout and styling for better user experience

--- vagas/app/not-found.tsx
+++ vagas/app/not-found.tsx
@@ -3,9 +3,10 @@ import Image from 'next/image';
 
 export default function NotFound() {
     return (
-        <div>
-            <Image src="/astronaut.png" alt="astrounauta" width={500} height={500} />
-            <p>Não foi possível encontrar a página solicitada.</p>
+        <div className="flex flex-col items-center justify-center h-screen space-y-4">
+            <h1 className="font-bold text-4xl p-4">Nada Encontrado Aqui!</h1>
+            <Image src="/astronaut.png" alt="astrounauta" width={600} height={600} />
+            <p className="font-normal text-2xl text-gray-500 mb-6">Não foi possível encontrar a página solicitada.</p>
             <Button>Voltar à Home</Button>
         </div>
     )
