1c39dbda9671244d02fefb52bc3501df35dd20ab
thelittlebirdbtc <thelittlebirdbtc@outlook.com>
2025-03-05 16:29:02 -0300

    Refactor NotFound component; add button and image for better user experience

--- vagas/app/not-found.tsx
+++ vagas/app/not-found.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function NotFound() {
     return (
         <div>
             <h1>Nada encontrado aqui!</h1>
+            <img src="/astronaut.png" alt="Erro 404" />
+            <p>Não foi possível encontrar a página solicitada.</p>
+            <Button>Voltar à Home</Button>
         </div>
     )
 }
