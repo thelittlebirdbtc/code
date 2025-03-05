caa05b67ebcbf960ff352612736f34c8da174d3e
thelittlebirdbtc <thelittlebirdbtc@outlook.com>
2025-03-05 16:48:01 -0300

    Enhance NotFound component; update font styles and improve layout for better readability

--- vagas/app/not-found.tsx
+++ vagas/app/not-found.tsx
@@ -3,11 +3,11 @@ import Image from 'next/image';
 
 export default function NotFound() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen space-y-4">
-            <h1 className="font-bold text-4xl p-4">Nada Encontrado Aqui!</h1>
+        <div className="flex flex-col items-center justify-center h-screen space-y-4 font-display">
+            <h1 className="font-black text-4xl p-4">Nada Encontrado Aqui!</h1>
             <Image src="/astronaut.png" alt="astrounauta" width={500} height={500}/>
             <p className="font-normal text-2xl text-gray-500">Não foi possível encontrar a página solicitada.</p>
-            <Button className="mt-8 text-lg py-8 px-6">Voltar à Home</Button>
+            <Button className="mt-8 text-lg py-8 px-6 font-medium">Voltar à Home</Button>
         </div>
     )
 }
