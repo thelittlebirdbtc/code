725c9966ed83c02083654e25c943d9296b33702f
thelittlebirdbtc <thelittlebirdbtc@outlook.com>
2025-03-05 16:41:12 -0300

    Refactor NotFound component; adjust image size and button styling for improved layout

--- vagas/app/not-found.tsx
+++ vagas/app/not-found.tsx
@@ -5,9 +5,9 @@ export default function NotFound() {
     return (
         <div className="flex flex-col items-center justify-center h-screen space-y-4">
             <h1 className="font-bold text-4xl p-4">Nada Encontrado Aqui!</h1>
-            <Image src="/astronaut.png" alt="astrounauta" width={600} height={600} />
-            <p className="font-normal text-2xl text-gray-500 mb-6">Não foi possível encontrar a página solicitada.</p>
-            <Button>Voltar à Home</Button>
+            <Image src="/astronaut.png" alt="astrounauta" width={500} height={500}/>
+            <p className="font-normal text-2xl text-gray-500">Não foi possível encontrar a página solicitada.</p>
+            <Button className="mt-8 text-lg py-8 px-6">Voltar à Home</Button>
         </div>
     )
 }
