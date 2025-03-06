1cd95d079fa65c6576d4bbc0128c16b484544c67
thelittlebirdbtc <thelittlebirdbtc@outlook.com>
2025-03-06 11:43:29 -0300

    Refactor Navbar component to use Link elements for navigation and improve layout structure

--- vagas/components/layout/navbar.tsx
+++ vagas/components/layout/navbar.tsx
@@ -1,15 +1,18 @@
+import { buttonVariants } from "../ui/button";
+import { cn } from "../../lib/utils";
 import Logo from "./logo";
+import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav className="flex items-center justify-between py-6">
+        <nav className="mx-auto max-w-5xl flex items-center justify-between py-6">
             <Logo />
-            <ul className="flex items-center space-x-4 px-8">
-                <li>Home</li>
-                <li>Sobre</li>
-                <li>Vagas</li>
-                <li>Cadastrar Vagas</li>
-            </ul>
+            <main className="flex items-center space-x-4 px-8">
+                <Link className={cn(buttonVariants({variant:"link"}))} href="/">Home</Link>
+                <Link className={cn(buttonVariants({variant:"link"}))} href="/Sobre">Sobre</Link>
+                <Link className={cn(buttonVariants({variant:"link"}))} href="/Vagas">Vagas</Link>
+                <Link className={cn(buttonVariants({variant:"link"}))} href="/Vagas/Cadastro">Cadastrar Vagas</Link>
+            </main>
         </nav>
     )
-}   
+}  
