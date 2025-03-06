4c3080c67d5dd2c28b0d58a6e6666cc1495a2b81
thelittlebirdbtc <thelittlebirdbtc@outlook.com>
2025-03-06 12:03:41 -0300

    Refactor Footer and Navbar components for improved layout and structure; replace Link elements with anchor tags for navigation

--- vagas/components/layout/footer.tsx
+++ vagas/components/layout/footer.tsx
@@ -1,7 +1,22 @@
+import { Github, Instagram, Mail } from "lucide-react";
+import Logo from "./logo";
+
 export default function Footer() {
     return (
-        <footer>
-            
+        <footer className=" bg-black py-10 text-gray-300 mx-auto px-10">
+            <div className="justify-between max-w-5xl mx-auto flex">
+                <div className="max-w-sm">
+                    <Logo bg="dark" />
+                    <p className="text-sm mt-2 font-light font-display">
+                    O CodanteVagas conecta candidatos a empregos ideais, oferecendo funcionalidades intuitivas para busca e gerenciamento de vagas.
+                    </p>
+                </div>
+                <div className="flex space-x-4">
+                    <Instagram />
+                    <Github />
+                    <Mail />
+                </div>
+            </div>
         </footer>
     )
 }
--- vagas/components/layout/navbar.tsx
+++ vagas/components/layout/navbar.tsx
@@ -1,17 +1,16 @@
 import { buttonVariants } from "../ui/button";
 import { cn } from "../../lib/utils";
 import Logo from "./logo";
-import Link from "next/link";
 
 export default function Navbar() {
     return (
         <nav className="mx-auto max-w-5xl flex items-center justify-between py-6">
             <Logo />
             <main className="flex items-center space-x-4 px-8">
-                <Link className={cn(buttonVariants({variant:"link"}))} href="/">Home</Link>
-                <Link className={cn(buttonVariants({variant:"link"}))} href="/Sobre">Sobre</Link>
-                <Link className={cn(buttonVariants({variant:"link"}))} href="/Vagas">Vagas</Link>
-                <Link className={cn(buttonVariants({variant:"link"}))} href="/Vagas/Cadastro">Cadastrar Vagas</Link>
+                <a className={cn(buttonVariants({variant:"link"}))} href="/Home">Home</a>
+                <a className={cn(buttonVariants({variant:"link"}))} href="/Sobre">Sobre</a>
+                <a className={cn(buttonVariants({variant:"link"}))} href="/Vagas">Vagas</a>
+                <a className={cn(buttonVariants({variant:"link"}))} href="/Vagas/Cadastro">Cadastrar Vagas</a>
             </main>
         </nav>
     )
