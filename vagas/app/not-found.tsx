edbdbebf06c09d4fde5ebcc902b1d19b45b3a278
thelittlebirdbtc <thelittlebirdbtc@outlook.com>
2025-03-06 10:45:36 -0300

    Refactor NotFound component; replace Button with Link for improved navigation and update layout structure

--- vagas/app/not-found.tsx
+++ vagas/app/not-found.tsx
@@ -1,13 +1,14 @@
-import { Button } from "@/components/ui/button";
+import { buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function NotFound() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen space-y-4 font-display">
+        <main className="flex flex-col items-center justify-center h-screen space-y-4 font-display">
             <h1 className="font-black text-4xl p-4">Nada Encontrado Aqui!</h1>
             <Image src="/astronaut.png" alt="astrounauta" width={500} height={500}/>
-            <p className="font-normal text-2xl text-gray-500">Não foi possível encontrar a página solicitada.</p>
-            <Button className="mt-8 text-lg py-8 px-6 font-medium">Voltar à Home</Button>
-        </div>
+            <Link className={cn('mt-10 block', buttonVariants({ variant: 'default' }))} href="/">Voltar à Home</Link>
+        </main>
     )
 }
