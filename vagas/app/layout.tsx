b5e8650de54d8224b6519ceb004b0f33582bec04
thelittlebirdbtc <thelittlebirdbtc@outlook.com>
2025-03-07 16:55:03 -0300

    Update global styles to use Maven Pro font and apply it in the layout component

--- vagas/app/layout.tsx
+++ vagas/app/layout.tsx
@@ -2,6 +2,15 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/layout/navbar";
 import Footer from "@/components/layout/footer";
+import { Maven_Pro } from "next/font/google";
+
+const mavenPro = Maven_Pro (
+{
+  weight: "variable", 
+  subsets: ["latin"], 
+  variable: "--font-maven-pro"
+}
+);
 
 export const metadata: Metadata = {
   title: "CodanteVagas",
@@ -15,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={mavenPro.variable}>
   <body>
     <div className="flex min-h-screen flex-col">
       <Navbar />
