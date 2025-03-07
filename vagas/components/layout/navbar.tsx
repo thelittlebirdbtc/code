284d696949013c62a7a76491d98488537a9e2193
thelittlebirdbtc <thelittlebirdbtc@outlook.com>
2025-03-07 16:31:35 -0300

    Refactor Navbar component to use Link from Next.js for navigation

--- vagas/components/layout/navbar.tsx
+++ vagas/components/layout/navbar.tsx
@@ -1,6 +1,7 @@
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '../ui/button';
 import Logo from './logo';
+import Link from 'next/link';
 
 export default function Navbar() {
   return (
@@ -8,31 +9,20 @@ export default function Navbar() {
       <div className="flex items-center">
         <Logo />
       </div>
-      <ul className="flex items-center gap-4">
-        <li>
-          <a className={cn(buttonVariants({ variant: 'link' }))} href="/ ">
-            Home
-          </a>
-        </li>
-        <li>
-          <a className={cn(buttonVariants({ variant: 'link' }))} href="/sobre">
-            Sobre
-          </a>
-        </li>
-        <li>
-          <a className={cn(buttonVariants({ variant: 'link' }))} href="/vagas">
-            Vagas
-          </a>
-        </li>
-        <li>
-          <a
-            className={cn(buttonVariants({ variant: 'link' }))}
-            href="/vagas/cadastro"
-          >
-            Cadastrar Vaga
-          </a>
-        </li>
-      </ul>
+      <main className="flex items-center gap-4">
+        <Link className={cn(buttonVariants({ variant: 'link' }))} href="/ ">
+          Home
+        </Link>
+        <Link className={cn(buttonVariants({ variant: 'link' }))} href="/sobre">
+          Sobre
+        </Link>
+        <Link className={cn(buttonVariants({ variant: 'link' }))} href="/vagas">
+          Vagas
+        </Link>
+        <Link className={cn(buttonVariants({ variant: 'link' }))} href="/vagas/cadastro">
+          Cadastrar Vaga
+        </Link>
+      </main>
     </nav>
   );
 }
