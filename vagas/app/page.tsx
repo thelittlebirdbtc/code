f6e736aa9cab13b30e2ae869014aa4006975b84b
thelittlebirdbtc <thelittlebirdbtc@outlook.com>
2025-03-07 10:46:22 -0300

    Refactor Button component in Home page for cleaner markup; update Navbar link href for consistency

--- vagas/app/page.tsx
+++ vagas/app/page.tsx
@@ -5,9 +5,7 @@ export default function Home() {
     <section className="text-center items-center flex flex-col justify-center gap-4 mt-8">
       <h1 className="text-5xl font-black font-display w-xl mx-auto">Diga adeus às longas buscas para uma vaga.</h1>
       <Image src="/people.png" alt="astronauta" width={500} height={500} />
-      <Button className="cursor-pointer mt-12" variant={'outline'}>
-        Busque Uma Vaga
-      </Button>
+      <Button className="cursor-pointer mt-12" variant={'outline'}>Busque Uma Vaga</Button>
     </section>
   );
 } 
