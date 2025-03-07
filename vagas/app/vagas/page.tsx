5597758a7fae9721288478cfcc9ee02f2372ca03
thelittlebirdbtc <thelittlebirdbtc@outlook.com>
2025-03-07 15:50:37 -0300

    Add JobItem component to display job listings with dynamic data

--- vagas/app/vagas/page.tsx
+++ vagas/app/vagas/page.tsx
@@ -1,7 +1,31 @@
+import JobItem from "@/components/cards/job-item";
+
+const job = {
+    id: "1",
+    tittle: "Desenvolvedor Frontend",
+    company: "Google",
+    city: "São Paulo",
+    salary: 10000,
+  }
+
 export default function Vagas() {
-    return (
-        <div>
-            <h1>Vagas</h1>
-        </div>
-    )
+  return (
+    <main className="py-10">
+      <h2 className="font-display mb-12 text-2xl font-bold">Todas as Vagas</h2>
+      <div className="space-y-8">
+        <JobItem job={job} />
+        <JobItem job={job} />
+        <JobItem job={job} />
+        <JobItem job={job} />
+        <JobItem job={job} />
+        <JobItem job={job} />
+        <JobItem job={job} />
+        <JobItem job={job} />
+        <JobItem job={job} />
+        <JobItem job={job} />
+        <JobItem job={job} />
+        <JobItem job={job} />
+      </div>
+    </main>
+  );
 }
--- vagas/components/cards/job-item.tsx
+++ vagas/components/cards/job-item.tsx
@@ -1,14 +1,24 @@
 import { Button } from "../ui/button";
 
-export default function JobItem() {
+type Job = {
+    id: string;
+    tittle: string;
+    company: string;
+    city: string;
+    salary: number;
+}
+
+type JobItemProps = {
+    job: Job;
+}
+
+export default function JobItem({ job }: JobItemProps) {
   return (
     <article className="flex w-full items-center justify-between border border-t-4 border-black px-6 py-4 transition-colors hover:border-blue-400">
-      <h3 className="font-display text-lg font-medium text-gray-700">
-        Designer
-      </h3>
-      <h4 className="font-light text-gray-500">Google</h4>
-      <h4 className="font-light text-gray-500">São Paulo</h4>
-      <h4 className="font-light text-gray-500">R$8.000,00</h4>
+      <h3 className="font-display text-lg font-medium text-gray-700">{job.tittle}</h3>
+      <h4 className="font-light text-gray-500">{job.company}</h4>
+      <h4 className="font-light text-gray-500">{job.city}</h4>
+      <h4 className="font-light text-gray-500">$ {job.salary}</h4>
       <Button className="cursor-pointer" variant={"outline"}>
         Mais
       </Button>
