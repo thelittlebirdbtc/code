bf475b34ce0848597ed055600504d4cf1facb9b0
thelittlebirdbtc <thelittlebirdbtc@outlook.com>
2025-03-08 11:31:08 -0300

    Enhance Job type definition with additional fields and add Badge and Separator components for UI consistency

--- vagas/lib/types.ts
+++ vagas/lib/types.ts
@@ -4,4 +4,9 @@ export type Job = {
     company: string;
     city: string;
     salary: number;
+    company_website: string;
+    schedule: "full time" | "part time";
+    number_of_positions: number;
+    description: string;
+    requirements: string;
 }
