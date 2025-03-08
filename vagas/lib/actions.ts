e309dc1f9ce085fff6945fed200368ef6f55a289
thelittlebirdbtc <thelittlebirdbtc@outlook.com>
2025-03-08 17:07:25 -0300

    Refactor deleteJob function to accept FormData for job ID retrieval

--- vagas/lib/actions.ts
+++ vagas/lib/actions.ts
@@ -2,9 +2,9 @@
 
 import { redirect } from "next/navigation";
 
-export async function deleteJob() {
+export async function deleteJob(formData: FormData) {
 
-    const jobId = "?";
+    const jobId = formData.get("id");
 
     const response = await fetch(`https://apis.codante.io/api/job-board/jobs/${jobId}`, {
         method: "DELETE",
